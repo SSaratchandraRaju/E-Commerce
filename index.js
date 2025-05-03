77298caba069d2b4ece7685a494249df8cc48ddf
SSaratchandraRaju <ssaratchandraraju@gmail.com>
2025-05-03 12:17:49 +0530

    Fixed Minor Bugs

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+// Select the MenuItems (ul) and set initial maxHeight to 0px (hidden)
 var MenuItems = document.getElementById('MenuItems');
-  MenuItems.style.maxHeight = '0px';
+MenuItems.style.maxHeight = '0px';
 
-  function menutoggle() {
-    if (MenuItems.style.maxHeight == '0px') {
-      MenuItems.style.maxHeight = '200px';
-    } else {
-      MenuItems.style.maxHeight = '0px';
-    }
+// Toggle function for opening/closing the menu
+function menutoggle() {
+  if (MenuItems.style.maxHeight == '0px') {
+    MenuItems.style.maxHeight = '200px';  // Adjust based on the number of menu items
+  } else {
+    MenuItems.style.maxHeight = '0px';
   }
+}
+
+// Select the menu icon and the menu list
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('nav ul');
+
+// Add an event listener to toggle the menu when the icon is clicked
+menuIcon.addEventListener('click', function() {
+  menuIcon.classList.toggle('active');
+  menu.classList.toggle('active');
+});
